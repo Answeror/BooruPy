1c1952b4f20a66495827ae752a1ff204c44ce245
answeror <answeror@gmail.com>
2013-06-29 21:41:52 +0800

    use python file as config

--- BooruPy/booru.py
+++ BooruPy/booru.py
@@ -9,18 +9,46 @@ from provider import DanbooruProvider, GelbooruProvider
 
 class BooruManager:
 
-    def __init__(self, providerlist_path, filter_nsfw=True):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def from_yaml(cls, providerlist_path, filter_nsfw=True):
+        inst = cls()
         providers = json.load(open(providerlist_path))
-        self.provider_list = []
+        inst.provider_list = []
         for p in providers["providers"]:
             if p["type"] == "danbooru":
-                self.provider_list.append(DanbooruProvider(p['url'], p['name'],
+                inst.provider_list.append(DanbooruProvider(p['url'], p['name'],
                     p['key'], filter_nsfw, p['mapping']))
             elif p["type"] == "gelbooru":
-                self.provider_list.append(GelbooruProvider(p['url'], p['name'],
+                inst.provider_list.append(GelbooruProvider(p['url'], p['name'],
                     p['key'], filter_nsfw, p['mapping']))
             else:
                 print("Unknown provider type: {0}".format(p["type"]))
+        return inst
+
+    @classmethod
+    def from_python_file(cls, path):
+        inst = cls()
+        inst.provider_list = []
+        import configit
+        conf = configit.from_file(path)
+        for p in conf.providers:
+            if p['type'] == 'danbooru':
+                Type = DanbooruProvider
+            elif p['type'] == 'gelbooru':
+                Type = GelbooruProvider
+            else:
+                raise Exception("Unknown provider type: {0}".format(p["type"]))
+            inst.provider_list.append(Type(
+                p['url'],
+                p['name'],
+                p['key'],
+                p.get('filter_nsfw', True),
+                p['mapping']
+            ))
+        return inst
 
     @property
     def providers(self):
